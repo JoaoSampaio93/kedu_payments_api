5544136ef06846a690dff98999366e1f4b2dd956
joao sampaio <joaomarcosemgp@gmail.com>
2025-11-26 18:37:30 -0300

    feat: add core domain models and relationships

--- app/models/plano_de_pagamento.rb
+++ app/models/plano_de_pagamento.rb
@@ -1,4 +1,9 @@
 class PlanoDePagamento < ApplicationRecord
   belongs_to :responsavel
   belongs_to :centro_de_custo
+  has_many :cobrancas
+
+  def valor_total
+    cobrancas.sum(:valor)
+  end
 end
