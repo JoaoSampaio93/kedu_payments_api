bd0433d5f9c8c3ef67ef98120c99990211377fef
joao sampaio <joaomarcosemgp@gmail.com>
2025-11-26 18:47:47 -0300

    feat: define REST routes for financial flows

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,16 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :responsaveis, only: %i[create show] do
+    resources :planos_de_pagamento, only: [:index]
+    get 'cobrancas', to: 'cobrancas#por_responsavel'
+    get 'cobrancas/quantidade', to: 'cobrancas#quantidade_por_responsavel'
+  end
+  resources :planos_de_pagamento, only: %i[create show] do
+    member do
+      get :total
+    end
+  end
+  resources :cobrancas, only: [] do
+    resources :pagamentos, only: :create
+  end
+  resources :centros_de_custo, only: %i[index create]
 end
